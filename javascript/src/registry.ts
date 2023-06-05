dee02fe56012ede34e100577c617bb8d945dffe0
Simon Liétar <simon@lietar.net>
2023-06-05 16:04:20 +0200

    Improve formatting

--- javascript/src/registry.ts
+++ javascript/src/registry.ts
@@ -1,10 +1,19 @@
 import { ReactNode } from 'react';
 
 import defaultRegistryData from '../data/registry.json';
-import { ConstantAssembly, ContextName, ContextVariant, ContextVariantOption, Data, SystemName } from './data.js';
+import { ConstantAssembly, Context, ContextName, ContextVariant, ContextVariantOption, Data, SystemName } from './data.js';
 
 
-export type CreateElementType<T> = (tag: string, attributes: Record<string, any> | null | undefined, ...children: ReactNode[]) => T;
+export type SerializedContext = {
+  type: 'known';
+  name: ContextName;
+} | {
+  type: 'anonymous';
+  value: Context;
+};
+
+export type CreateElementType<T> = (tag: string, attributes: Record<string, any> | null | undefined, ...children: Node<T>[]) => Node<T>;
+export type Node<T> = Iterable<Node<T>> | T | string;
 
 export class UnitRegistry {
   data: Data;
@@ -13,8 +22,23 @@ export class UnitRegistry {
     this.data = data;
   }
 
-  filterRangeCompositeUnitFormats(min: number, max: number, options: { context: ContextName; system: SystemName; }) {
-    let variant = this.findVariant(options);
+  getContext(context: Context | string) {
+    return (typeof context === 'string')
+      ? this.data.contexts[context]
+      : context;
+  }
+
+  deserializeContext(serializedContext: SerializedContext) {
+    switch (serializedContext.type) {
+      case 'known':
+        return this.data.contexts[serializedContext.name];
+      case 'anonymous':
+        return serializedContext.value;
+    }
+  }
+
+  filterRangeCompositeUnitFormats(min: number, max: number, context: Context, options: { system: SystemName; }) {
+    let variant = this.findVariant(context, { system: options.system });
     let minOption = this.findBestVariantOption(min, variant);
     let maxOption = this.findBestVariantOption(max, variant);
 
@@ -36,37 +60,52 @@ export class UnitRegistry {
     })[0];
   }
 
-  findVariant(options: { context: ContextName; system: SystemName; }) {
-    let context = this.data.contexts[options.context];
+  findVariant(context: Context, options: { system: SystemName; }) {
     return context.variants.find((variant) => variant.systems.includes(options.system))!;
   }
 
-  formatQuantityAsReact<T>(value: number, resolution: number, option: ContextVariantOption, options: {
+  formatQuantityAsReact<T>(value: number, resolution: number, context: Context | string, options: {
     createElement: CreateElementType<T>;
-    skipUnit?: boolean;
+    sign?: unknown;
+    style?: 'label' | 'symbol';
   }) {
+    let variant = this.findVariant(this.getContext(context), { system: 'SI' });
+    let option = this.findBestVariantOption(value, variant);
+
+    return this.formatQuantityWithContextAsReact(value, resolution, option, options);
+  }
+
+  formatQuantityWithContextAsReact<T>(value: number, resolution: number, option: ContextVariantOption, options: {
+    createElement: CreateElementType<T>;
+    sign?: unknown;
+    skipUnit?: boolean;
+    style?: 'label' | 'symbol';
+  }): [Node<T>, Node<T>, Node<T>] & Node<T> {
     return [
-      ...this.formatMagnitudeAsReact(value / option.value, resolution / option.value),
+      this.formatMagnitudeAsReact(value / option.value, resolution / option.value, { sign: options.sign }),
       ...(!options.skipUnit && (option.assembly.length > 0)
         ? [
-          '&nbsp;',
-          ...this.formatAssemblyAsReact(option.assembly, { createElement: options.createElement })
-        ]
-        : [])
+          '\xa0', // &nbsp;
+          this.formatAssemblyAsReact(option.assembly, {
+            createElement: options.createElement,
+            style: options.style
+          })
+        ] as const
+        : [[], []] as const)
     ];
   }
 
   formatAssemblyAsReact<T>(assembly: ConstantAssembly, options: {
     createElement: CreateElementType<T>;
-    style?: 'label' | 'symbol';
-  }) {
-    let output: (T | string)[] = [];
+    style?: 'label' | 'symbol' | undefined;
+  }): Node<T> {
+    let output: (Node<T> | string)[] = [];
 
     for (let [index, [unitId, power]] of assembly.entries()) {
       let unit = this.data.units[unitId];
 
       if (index > 0) {
-        output.push((power < 0 ? '/' : '&middot;'));
+        output.push((power < 0 ? '/' : '\xb7')); // &middot;
       }
 
       let plural = (index < 1) && (power > 0);
@@ -90,37 +129,24 @@ export class UnitRegistry {
     return output;
   }
 
-  formatMagnitudeAsReact(value: number, resolution: number) {
+  formatMagnitudeAsReact(value: number, resolution: number, options?: { sign?: unknown; }) {
     let decimalCount = Math.max(0, Math.ceil(-Math.log10(resolution)));
 
     return [
-      ...(value < 0 ? ['&ndash;'] : []),
+      ...((value < 0)
+        ? ['\u2212\u2009'] // &minus; &thinsp;
+        : options?.sign
+          ? ['+\u2009']
+          : []),
       Math.abs(value).toFixed(isFinite(decimalCount) ? decimalCount : 0)
     ];
   }
 
   formatRangeAsReact<T>(min: number, max: number, resolution: number, minOption: ContextVariantOption, maxOption: ContextVariantOption, options: { createElement: CreateElementType<T>; }) {
     return [
-      ...this.formatQuantityAsReact(min, resolution, minOption, { ...options, skipUnit: (maxOption === minOption) }),
-      ' &mdash; ',
-      ...this.formatQuantityAsReact(max, resolution, maxOption, options)
+      ...this.formatQuantityWithContextAsReact(min, resolution, minOption, { ...options, skipUnit: (maxOption === minOption) }),
+      ' \u2014 ', // &mdash;
+      ...this.formatQuantityWithContextAsReact(max, resolution, maxOption, options)
     ];
   }
 }
-
-
-let ureg = new UnitRegistry();
-
-let value = 0.5;
-let variant = ureg.findVariant({ context: 'velocity', system: 'SI' });
-let option = ureg.findBestVariantOption(value, variant);
-
-console.log(ureg.formatQuantityAsReact(value, 0.1, option, {
-  createElement: (...args) => args
-}));
-
-console.log(ureg.formatRangeAsReact(2.0, 5000.0, 0.,
-  ureg.findBestVariantOption(2.0, variant),
-  ureg.findBestVariantOption(5000.0, variant),
-  { createElement: (...args) => args }
-));
