9704ccb9a8438ffc5bd96ef7a01704dbac7e6548
Simon Liétar <simon@lietar.net>
2024-11-04 17:59:37 +0100

    Improve type interface generation

--- python/quantops/generate_types.py
+++ python/quantops/generate_types.py
@@ -1,4 +1,6 @@
-from .core import AtomicUnit, UnitRegistry
+import ast
+
+from .core import AtomicUnit, Quantity, UnitRegistry
 
 
 def generate_types(name: str, ureg: UnitRegistry):
@@ -12,8 +14,37 @@ def generate_types(name: str, ureg: UnitRegistry):
 
 
   for extent in ureg._extents_by_dimensionality.values():
+    ## Unit class
+
+    output += f'class {extent.unit_symbol}({AtomicUnit.__name__}):\n'
+
+    output += f'  def __mul__(self, other: float) -> {extent.quantity_symbol}:\n    ...\n\n'
+
+    for other_extent in ureg._extents_by_dimensionality.values():
+      cross_dimensionality = extent.value * other_extent.value
+      cross_extent = ureg._extents_by_dimensionality.get(cross_dimensionality)
+
+      if cross_extent:
+        output += f'  @overload\n'
+        output += f'  def __mul__(self, other: {other_extent.unit_symbol}) -> {cross_extent.unit_symbol}:\n    ...\n\n'
+
+
+    # output += f'  def __mul__(self, other: {Quantity.__name__}) -> {Quantity.__name__}:\n    ...\n\n'
+
+    output += '\n'
+
+
+
+    ## Quantity class
+
     output += f'class {extent.quantity_symbol}:\n'
+
+    # Addition, substrction
+
     output += f'  def __add__(self, other: {extent.quantity_symbol}) -> {extent.quantity_symbol}:\n    ...\n\n'
+    output += f'  def __sub__(self, other: {extent.quantity_symbol}) -> {extent.quantity_symbol}:\n    ...\n\n'
+
+    # Multiplication
 
     output += f'  @overload\n'
     output += f'  def __mul__(self, other: float) -> {extent.quantity_symbol}:\n    ...\n\n'
@@ -26,9 +57,23 @@ def generate_types(name: str, ureg: UnitRegistry):
         output += f'  @overload\n'
         output += f'  def __mul__(self, other: {other_extent.quantity_symbol}) -> {cross_extent.quantity_symbol}:\n    ...\n\n'
 
+    # Division
+
+    output += f'  @overload\n'
+    output += f'  def __truediv__(self, other: float) -> {extent.quantity_symbol}:\n    ...\n\n'
+
+    for other_extent in ureg._extents_by_dimensionality.values():
+      cross_dimensionality = extent.value / other_extent.value
+      cross_extent = ureg._extents_by_dimensionality.get(cross_dimensionality)
+
+      if cross_extent:
+        output += f'  @overload\n'
+        output += f'  def __truediv__(self, other: {other_extent.quantity_symbol}) -> {cross_extent.quantity_symbol}:\n    ...\n\n'
+
 
   return output
 
 
-# x = generate_types('DefaultRegistry', UnitRegistry.load_default())
-# print(x)
+x = generate_types('DefaultRegistry', UnitRegistry.load_default())
+
+print(x)
